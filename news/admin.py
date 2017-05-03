28630c2df5b9182fa0411ac725e5d36b1d959c63
zhaoR <guangking1987@163.com>
2017-05-03 18:06:59 +0800

    4

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from news.models import Event,Guest
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name','status','start_time','id']
+    search_fields = ['name']
+    list_filter = ['status']
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname','phone','email','sign','create_time','event']
+    search_fields = ['realname','phone']
+    list_filter = ['sign']
+
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(Guest,GuestAdmin)
