c81fda76603595b2d86537af1d1fd99be50a5315
zhaoR <guangking1987@163.com>
2017-05-04 14:17:25 +0800

    5

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls import url
 from django.contrib import admin
-from news.views import login_action,event_manage
+from news.views import login_action,event_manage,search_name,guest_manage
 urlpatterns = [
     url(r'^login_action/',login_action),
     url(r'^event_manage/',event_manage),
+    url(r'^search_name/',search_name),
+    url(r'^guest_manage/',guest_manage)
    ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from news.models import Event
+from news.models import Event,Guest
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def index(request):
     return render(request,"sign/index.html")
@@ -14,7 +15,7 @@ def login_action(request):
         if user is not None:
             auth.login(request,user)
             request.session['user']=username
-            respone= HttpResponseRedirect('/news/event_manage/')
+            respone = HttpResponseRedirect('/news/event_manage/')
             return respone
         else:
             return render(request,'sign/index.html',{'error':'username or password error!'})
@@ -24,6 +25,27 @@ def event_manage(request):
     event_list=Event.objects.all()
     username= request.session.get('user','')
     return render(request,"sign/event_manage.html",{"user":username,"events":event_list})
+@login_required
+def search_name(request):
+    username= request.session.get('user','')
+    search_name= request.GET.get('name','')
+    event_list= Event.objects.filter(name__contains=search_name)
+    return render(request,"sign/event_manage.html",{"user":username,"events":event_list})
+
+@login_required
+def guest_manage(request):
+    username= request.session.get('user','')
+    guest_list= Guest.objects.all()
+    paginator= Paginator(guest_list,2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        contacts= paginator.page(1)
+    except EmptyPage:
+        contacts=paginator.page(paginator.num_pages)
+    return render(request,"sign/guest_manage.html",{"user":username,"guests":contacts})
+
 
 @login_required
 def logout(request):
